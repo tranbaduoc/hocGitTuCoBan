f33c4053e5b5fc5df5c19f36b369b86babb88e23
Được Trần <62225713+tranbaduoc@users.noreply.github.com>
2020-07-09 16:34:55 +0700

    update gui

--- TuDien/AnhViet/Form1.Designer.cs
+++ TuDien/AnhViet/Form1.Designer.cs
@@ -34,12 +34,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(162, 65);
+            this.button1.Location = new System.Drawing.Point(100, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
